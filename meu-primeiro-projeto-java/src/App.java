236ec11b0483f70b3819e832cb23ae5e8ad58f31
reinaldofojr <mail.reinaldojunior@gmail.com>
2024-02-07 10:13:01 -0300

    Teste commit

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Boas vindas!!!");
+        System.out.println("Boas vindas!!! Entendendo o que é isso");
     }
 } 
